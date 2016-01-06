e7296c999b608cac04439c91ead59eff504e536f
pasaunders <kaorti@gmail.com>
2016-01-06 13:21:00 -0800

    basic error correction and number guessing loop

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var user = prompt('Tell me your name.');
 console.log('The user\'s name is ' + user);
 
 alert ('My name is Patrick, '+ user +', what do you know about me? Let\'s find out.');
-alert ('Each questions is a yes or no question. Please answer only with a lower case y or n.');
+alert ('The first three questions are yes or no questions. The last question asks you to guess a number');
 
 var questionsRight = 0;
 
 var answer1 = prompt('Patrick has more board games than he has played in the past year. y/n');
 console.log('The user answered question 1: '+answer1);
-if (answer1 === 'y' || answer1 === 'Y' || answer1 === 'yes' || answer1 === 'YES') {
+if (answer1.toLowerCase() === 'y' || answer1.toLowerCase() === 'yes') {
   console.log('question 1 answered yes');
   alert('It\'s a pity but you\'re right, ' + user +', I don\'t play enough.');
   questionsRight++;
@@ -20,7 +20,7 @@ if (answer1 === 'y' || answer1 === 'Y' || answer1 === 'yes' || answer1 === 'YES'
 
 var answer2 = prompt('Patrick\'s favorite drink is a Bloody Mary. y/n');
 console.log('The user answered question 2: '+answer2);
-if (answer2 === 'n' || answer2 === 'N' || answer2 === 'no' || answer2 === 'NO') {
+if (answer2.toLowerCase() === 'n' || answer2.toLowerCase() === 'no') {
   console.log('question 2 answered no');
   alert('Good guess, ' +user+', Patrick\'s favorite drink is a Corpse Reviver No.3.');
   questionsRight++;
@@ -32,7 +32,7 @@ if (answer2 === 'n' || answer2 === 'N' || answer2 === 'no' || answer2 === 'NO') 
 
 var answer3 = prompt('West coast best coast? y/n');
 console.log('The user answered question 3: '+answer3);
-if (answer3 === 'y' || answer3 === 'Y' || answer3 === 'yes' || answer3 === 'YES') {
+if (answer3.toLowerCase() === 'y' || answer3.toLowerCase() === 'yes') {
   console.log('question 3 answered yes');
   alert('You know it, ' +user+'.');
   questionsRight++;
@@ -42,4 +42,24 @@ if (answer3 === 'y' || answer3 === 'Y' || answer3 === 'yes' || answer3 === 'YES'
   alert('You poor fool, ' +user+'.');
 }
 
-alert(user + ' got ' + questionsRight + ' questions right.');
+do {
+  console.log('dowhile loop');
+  var rightYet = false;
+  answer4 = prompt('One last question ' +user+'. What number am I thinking of? I\'ll give you a hint. It\'s between 1 and 10.');
+  if (answer4 == 4) {
+    console.log('just right');
+    alert('DING DING DING! Got it!');
+    questionsRight++;
+    rightYet = true;
+  }
+  else if (answer4 < 4) {
+    alert('Too low, try again ' +user+ '.');
+    console.log('too low');
+  }
+  else {
+    alert('Too high, try again ' +user+ '.');
+    console.log('too high');
+  }
+} while (rightYet !== true );
+
+alert('You got ' + questionsRight + ' question right ' +user+ '.');
