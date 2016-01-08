61fbe28aa16d0a072c94ed2dd0b76c2c8648aa51
pasaunders <kaorti@gmail.com>
2016-01-08 08:53:49 -0800

    cleaned up the last alerts, added the missing fourth question.

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var res1 = document.getElementById('resultOne');
 var res2 = document.getElementById('resultTwo');
 var res3 = document.getElementById('resultThree');
 var res4 = document.getElementById('resultFour');
+var res5 = document.getElementById('resultFive');
+var res6 = document.getElementById('correctCounter')
 
 var user = prompt('Tell me your name.');
 console.log('The user\'s name is ' + user);
 
 alert ('My name is Patrick, ' + user + ', what do you know about me? Let\'s find out.');
-alert ('The first three questions are yes or no questions. The last question asks you to guess a number');
-
 var questionsRight = 0;
 
 /* First question. yes or no */
@@ -62,40 +62,62 @@ if (answer3.toLowerCase() === 'y' || answer3.toLowerCase() === 'yes') {
 }
 }
 
-/* Fourth question, loops until the user guesses right, gives a hint each time */
+//fourth question, using numbers instead of strings
 function questionFour() {
+  var answer4 = prompt('Guess a number betwee one and 10. You only get one try.');
+console.log('The user answered question 4: '+answer4);
+if (answer4 === '3') {
+  console.log('question 4 answered right');
+  // alert('You know it, ' + user + '.');
+  res4.textContent = 'You know it, ' + user + '.';
+  questionsRight++;
+  console.log('number of questions right so far: ' + questionsRight);
+} else {
+  console.log('question 4 answered wrong');
+  // alert('You poor fool, ' + user + '.');
+  res4.textContent = 'Wrong, ' + user + '.';
+}
+}
+
+/* Fifth question, loops until the user guesses right, gives a hint each time */
+function questionFive() {
   do {
   console.log('dowhile loop');
   var rightYet = false;
-  var answer4 = prompt('One last question ' + user + '. What number am I thinking of? I\'ll give you a hint. It\'s between 1 and 10.');
-  if (answer4 == 4) {
+  var answer5 = prompt('One last question ' + user + '. What number am I thinking of? I\'ll give you a hint. It\'s between 1 and 10.');
+  if (answer5 == 4) {
     console.log('just right');
     // alert('DING DING DING! Got it!');
-    res4.textContent = 'DING DING DING! Got it!';
+    res5.textContent = 'DING DING DING! Got it!';
     questionsRight++;
     rightYet = true;
   }
-  else if (answer4 < 4) {
+  else if (answer5 < 4) {
     // alert('Too low, try again ' + user + '.');
     console.log('too low');
-    res4.textContent = 'Too low, try again ' + user + '.';
+    res5.textContent = 'Too low, try again ' + user + '.';
   }
-  else if (answer4 > 4) {
+  else if (answer5 > 4) {
     // alert('Too high, try again ' + user + '.');
-    res4.textContent = 'Too high, try again ' + user + '.';
+    res5.textContent = 'Too high, try again ' + user + '.';
     console.log('too high');
   }
   else {
   // alert('I\'m just a dumb computer. Please give your answer as a numeral.')
-  res4.textContent = 'I\'m just a dumb computer. Please give your answer as a numeral.';
+  res5.textContent = 'I\'m just a dumb computer. Please give your answer as a numeral.';
   }
 } while (rightYet !== true );
+}
+
+
+/* Tells the user how many questions he or she got right */
+function numberRight(){
+  res6.textContent = 'You got ' + questionsRight + ' question right ' + user + '.';
 }
 
 questionOne();
 questionTwo();
 questionThree();
 questionFour();
-
-/* Tells the user how many questions he or she got right */
-alert('You got ' + questionsRight + ' question right ' + user + '.');
+questionFive();
+numberRight();
